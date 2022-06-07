5813150ad25393415881dc82c2761f30665dac96
Felix Mckenzie <felixmckenzie90@gmail.com>
2022-06-07 10:50:09 +1000

    added format_condition helper

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,10 @@
 module ListingsHelper
+   
+    def format_condition(condition)
+       words = condition.split("_").map do |word| 
+            word.capitalize 
+        end
+        words.join(" ")
+    end
 end
+
